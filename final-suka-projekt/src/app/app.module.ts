7290ea81c383a659d104a78aba1810c6f4f7394b
Dmytro Myloserdov <dagger.dager@gmail.com>
2019-07-02 06:14:39 -0700

    Minor fixes

--- final-suka-projekt/src/app/app.module.ts
+++ final-suka-projekt/src/app/app.module.ts
@@ -2,11 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ComponentComponent } from './component/component.component';
-import { HomeComponent } from './component/home/home.component';
 import { TopNavComponent } from './header/top-nav/top-nav.component';
 import { HeaderContentComponent } from './header/header-content/header-content.component';
 import { HeaderDropdownComponent } from './header/header-dropdown/header-dropdown.component';
@@ -14,11 +12,9 @@ import { HeaderDropdownComponent } from './header/header-dropdown/header-dropdow
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
     HeaderComponent,
     FooterComponent,
     ComponentComponent,
-    HomeComponent,
     TopNavComponent,
     HeaderContentComponent,
     HeaderDropdownComponent
